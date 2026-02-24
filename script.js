20f1ed326a8d4c7b391e506a37e0fc9649d4d4ce
gaurijain27 <gaurijain.2720@gmail.com>
2026-02-24 09:43:17 +0530

    primitive data types in js

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-console.log("hello World");
-document.write("HELLO WORLD");
+//primitive data types in javascript
+let num;
+console.log(num);
+console.log(typeof(num));
+num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="Gauri Jain";
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+
+
+const sym1= Symbol(4); //symbol type
+console.log(sym1);
+const sym2= Symbol(4);
+console.log(sym1);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
